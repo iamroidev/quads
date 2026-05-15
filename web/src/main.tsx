fa7cfa20cd3c8260c12b6ddbc3cbbcb26cf672ae
amaroidev <richieroigeek@gmail.com>
2026-05-15 15:35:11 +0000

    Fix missing CartProvider import + useCart import

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Toaster } from 'react-hot-toast';
 import { AuthProvider } from './context/AuthContext';
+import { CartProvider } from './context/CartContext';
 import { SocketProvider } from './context/SocketContext';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import App from './App';
