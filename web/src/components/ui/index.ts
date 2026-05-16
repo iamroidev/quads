98f50ff1b53531a46b46a3c2a43a85d8dcb0cfd1
amaroidev <richieroigeek@gmail.com>
2026-05-16 00:39:11 +0000

    Fix: Replaced emojis with monochrome Lucide icons for institutional bulletin style

--- web/src/components/ui/index.ts
+++ web/src/components/ui/index.ts
@@ -1,3 +1,7 @@
 export { default as Button } from './Button';
 export { default as Input } from './Input';
 export { default as LoadingSpinner } from './LoadingSpinner';
+export * from './CategoryIcon';
+export * from './BulletinMarquee';
+export * from './BulletinEmptyState';
+export * from './BulletinSkeleton';
